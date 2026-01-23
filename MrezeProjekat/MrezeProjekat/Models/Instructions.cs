b9b96116e32e4d72444cf3f43602361e6568c8ba
Danijel Musli <danijelmusli56@gmail.com>
2026-01-23 17:02:14 +0100

    Implementing base classes

--- MrezeProjekat/MrezeProjekat/Models/Instructions.cs
+++ MrezeProjekat/MrezeProjekat/Models/Instructions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,25 @@ namespace MrezeProjekat.Models
 {
     internal class Instructions
     {
+        private string[] _keys;
+        private IPEndPoint _prevNode;
+        private IPEndPoint _nextNode;
+
+        public Instructions(int keyNum, IPEndPoint prev, IPEndPoint next) 
+        {
+            this._keys = new string[keyNum];
+            this._prevNode = prev;  
+            this._nextNode = next;
+        }
+
+        public string this[int index]
+        {
+            get => this._keys[index];
+            set => this._keys[index] = value;
+        }
+
+        public IPEndPoint PrevNode => this._prevNode;
+        public IPEndPoint NextNode => this._nextNode;
+
     }
 }
--- MrezeProjekat/MrezeProjekat/Models/Request.cs
+++ MrezeProjekat/MrezeProjekat/Models/Request.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace MrezeProjekat.Models
 {
     internal class Request
     {
+        private IPEndPoint _sender;
+        private int _nodeNum;
+        private int _maxMessages; // Maksimalni broj poruka, nakon kog se lanac prekida 
+
+        public Request() { }
+        public Request(IPEndPoint senderPoint, int nodeNumber, int maxMessagesNum)
+        {
+            this._sender = senderPoint;
+            this._nodeNum = nodeNumber;
+            this._maxMessages = maxMessagesNum;
+        }
+
+        public IPEndPoint Sender => this._sender;
+        public int NodeNum => this._nodeNum;
+        public int MaxMessages => this._maxMessages;
+
     }
 }
